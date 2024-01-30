07bbcf7afe672ae3094cb0f0d469740d77bb2c46
김세은 <137882823+kimseeun10@users.noreply.github.com>
2024-01-31 01:52:51 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 17960 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double A = sc.nextDouble();
+		double B = sc.nextDouble();
+		
+		sc.close();
+		
+		double C = A/B;
+		
+		System.out.println(C);
+				
+	}
+
+}
