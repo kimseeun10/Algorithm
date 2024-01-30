83b4c3d6f3b3c8454605bdd4b24e40cf05f300a6
김세은 <137882823+kimseeun10@users.noreply.github.com>
2024-01-31 01:59:45 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		String id = sc.next();
+		
+		sc.close();
+		
+		String emo = "??!";
+		
+		System.out.println(id+emo);
+				
+	}
+
+}
