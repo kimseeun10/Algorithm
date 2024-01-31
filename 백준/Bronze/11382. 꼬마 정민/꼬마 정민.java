1cef0c7fd80581031d53471d52627d082651cb10
김세은 <137882823+kimseeun10@users.noreply.github.com>
2024-01-31 20:01:36 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+			
+		Long A = sc.nextLong();
+		Long B = sc.nextLong();
+		Long C = sc.nextLong();
+		
+		sc.close();
+		
+		System.out.println(A+B+C);
+		
+	}
+}
