eefeac4bf0d93a7873fdb4998d62881ca88794be
김세은 <137882823+kimseeun10@users.noreply.github.com>
2024-02-14 21:46:43 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 224 ms, Memory: 18328 KB -BaekjoonHub

---
+++ 백준/Bronze V/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int [] chess = {1,1,2,2,2,8}; 
+		
+		int result = 0;
+		
+		for(int i=0; i<chess.length; i++) {
+			int count = sc.nextInt();
+			
+			result = chess[i] - count;
+			
+			System.out.print(result + " ");
+		}
+		
+		sc.close();
+		
+	}
+	
+}
